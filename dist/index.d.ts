4a2b67e0c9ada70f4c847f3e295336add26dee24
Levy <levy@codeandmore.pl>
2018-03-15 15:40:32 +0100

    Add currency calculator to barrel

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ export { CurrencyAndNumber, IntlHelper } from "./helper";
 export { Country } from "./country";
 export { Money } from "./money";
 export { Currency } from "./currency";
+export { CurrencyCalculator } from "./currency-calculator";
 export { importMessages } from "./messages";
