2164d3330315e7c7b98d8e36077ea4018a92fe56
Levy <levy@codeandmore.pl>
2019-10-25 22:05:03 +0200

    Commit

---
+++ dist/country-bundle-item.d.ts
@@ -0,0 +1,2 @@
+import { IntlBundleItem } from "./bundle-generator";
+export default function countryIntlBundleItem(node_modules?: string): IntlBundleItem;
---
+++ dist/esm/country-bundle-item.js
@@ -0,0 +1,4 @@
+export default function countryIntlBundleItem(node_modules = "node_modules") {
+    return { path: `${node_modules}/@umpirsky/country-list/data/{{LOCALE}}/country.json`, type: "message", namespace: "@umpirsky/country-list" };
+}
+//# sourceMappingURL=country-bundle-item.js.map
