b08954280e68fc185dfc9432ef0bba7e6836fabd
MarkChrisLevy <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>
2022-09-17 16:03:33 +0200

    commit

--- dist/IntlContext.d.ts
+++ dist/IntlContext.d.ts
@@ -3,7 +3,7 @@ export declare abstract class IntlContext {
     abstract get locales(): string[];
     abstract get defaultNamespace(): string;
     private predefinedFormatOptions;
-    private addPredefinedFormatOptions;
+    addPredefinedFormatOptions<T>(name: string, options: any): void;
     findPredefinedFormatOptions(name: string): any;
     /**
      * Path or url to a directory, where intl resources are stored.
